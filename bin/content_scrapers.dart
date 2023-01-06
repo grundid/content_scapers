b198dc511b7599096ce6b1636c0f2af66127c559
Adrian Stabiszewski <as@grundid.de>
2023-01-06 23:43:53 +0100

    move config data to properties file

--- bin/content_scrapers.dart
+++ bin/content_scrapers.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
+import 'package:gradle_properties/gradle_properties.dart';
 import 'package:html/dom.dart';
 import 'package:html/parser.dart';
 
@@ -20,73 +23,91 @@ List<String> getPages(Document document) {
   return result;
 }
 
-String baseUrl = "https://www.gundelsheim.de";
-String firstPage = "/rathaus-service/aktuelle-meldungen";
-String channelId = "63b733f34f0c81661d73709a";
+void run(String configFile, Dio dio) async {
+  GradleProperties? properties =
+      await GradleProperties.fromFile(File(configFile));
+  if (properties == null) {
+    print("Missing config.properties");
+  } else {
+    String baseUrl = properties["baseUrl"]!;
+    String firstPage = properties["firstPage"]!;
+    String channelId = properties["channelId"]!;
 
-void run(Dio dio) async {
-  RegExp regExp = RegExp("(\\d\\d)\\.(\\d\\d)\\.(\\d\\d\\d\\d)");
+    RegExp regExp = RegExp("(\\d\\d)\\.(\\d\\d)\\.(\\d\\d\\d\\d)");
 
-  List<Document> documents = [
-    //parse(File("data/aktuelle-meldungen.html").readAsStringSync())
-  ];
+    List<Document> documents = [
+      //parse(File("data/aktuelle-meldungen.html").readAsStringSync())
+    ];
 
-  print("loading first page");
-  Response response = await dio.get(baseUrl + firstPage);
-  if (response.statusCode == 200) {
-    Document document = parse(response.data);
-    documents.add(document);
-    List<String> pages = getPages(document);
-    for (String pageUrl in pages) {
-      print("loading " + baseUrl + pageUrl);
-      Response pageResponse = await dio.get(baseUrl + pageUrl);
-      if (pageResponse.statusCode == 200) {
-        documents.add(parse(pageResponse.data));
+    print("loading first page");
+    Response response = await dio.get(baseUrl + firstPage);
+    if (response.statusCode == 200) {
+      Document document = parse(response.data);
+      documents.add(document);
+      List<String> pages = getPages(document);
+      for (String pageUrl in pages) {
+        print("loading " + baseUrl + pageUrl);
+        Response pageResponse = await dio.get(baseUrl + pageUrl);
+        if (pageResponse.statusCode == 200) {
+          documents.add(parse(pageResponse.data));
+        }
       }
     }
-  }
-  List<Map<String, dynamic>> contentItems = [];
+    List<Map<String, dynamic>> contentItems = [];
 
-  for (Document document in documents) {
-    for (Element element in document.getElementsByClassName("record")) {
-      Element title = element.getElementsByTagName("h3").first;
-      Element description = element.getElementsByClassName("description").first;
-      Element link = element.getElementsByClassName("button_continue").first;
-      Element img = element.getElementsByClassName("teaserimage").first;
-      Element pubDate = element.getElementsByClassName("creation").first;
+    for (Document document in documents) {
+      for (Element element in document.getElementsByClassName("record")) {
+        Element title = element.getElementsByTagName("h3").first;
+        Element description =
+            element.getElementsByClassName("description").first;
+        Element link = element.getElementsByClassName("button_continue").first;
+        Element img = element.getElementsByClassName("teaserimage").first;
+        Element pubDate = element.getElementsByClassName("creation").first;
 
-      DateTime pubDateTime = DateTime.now();
-      RegExpMatch? regExpMatch = regExp.firstMatch(pubDate.text);
-      if (regExpMatch != null && regExpMatch.groupCount >= 3) {
-        pubDateTime = DateTime(int.parse(regExpMatch.group(3)!),
-            int.parse(regExpMatch.group(2)!), int.parse(regExpMatch.group(1)!));
+        DateTime pubDateTime = DateTime.now();
+        RegExpMatch? regExpMatch = regExp.firstMatch(pubDate.text);
+        if (regExpMatch != null && regExpMatch.groupCount >= 3) {
+          pubDateTime = DateTime(
+              int.parse(regExpMatch.group(3)!),
+              int.parse(regExpMatch.group(2)!),
+              int.parse(regExpMatch.group(1)!));
+        }
+
+        Map<String, dynamic> data = {
+          "title": title.text.trim(),
+          "link": "https://www.gundelsheim.de/${link.attributes["href"]}",
+          "pubDate": pubDateTime.toIso8601String(),
+          "author": "Gundelsheim",
+          "content": description.text.trim(),
+          "channelId": channelId
+        };
+        contentItems.add(data);
       }
-
-      Map<String, dynamic> data = {
-        "title": title.text.trim(),
-        "link": "https://www.gundelsheim.de/${link.attributes["href"]}",
-        "pubDate": pubDateTime.toIso8601String(),
-        "author": "Gundelsheim",
-        "content": description.text.trim(),
-        "channelId": channelId
-      };
-      contentItems.add(data);
     }
-  }
 
-  for (Map<String, dynamic> data in contentItems) {
-    print("sending item...");
-    try {
-      Response response =
-          await dio.put("https://api.grundid.de/rss/item", data: data);
-      print("status: ${response.statusCode}");
-      print(response.data);
-    } on DioError catch (_) {
-      break;
+    int sucessfull = 0;
+    for (Map<String, dynamic> data in contentItems) {
+      try {
+        Response response =
+            await dio.put(properties["storageUrl"]!, data: data);
+        if (response.statusCode == 200) {
+          sucessfull++;
+        }
+      } on DioError catch (_) {
+        break;
+      }
     }
+
+    Map<String, dynamic> confirmNotification = {
+      "subject": "$sucessfull von ${contentItems.length} Seiten gescraped.",
+      "message":
+          "Aktuelle Nachrichten:\n\n${contentItems.map((e) => e["title"]).join("\n")}"
+    };
+
+    await dio.post(properties["notificationUrl"]!, data: confirmNotification);
   }
 }
 
 void main(List<String> arguments) {
-  run(Dio());
+  run(arguments.first, Dio());
 }
